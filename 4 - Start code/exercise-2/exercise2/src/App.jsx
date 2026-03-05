283b34668504364293850d49389c7142751401f0
YamSoseereiwat <sosereiwat@gmail.com>
2026-03-05 14:51:56 +0700

    UPDATE

---
+++ 4 - Start code/exercise-2/exercise2/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Mains from "./Mains";
+import Footer from "./Footer";
+
+function App() { 
+  return ( 
+  <> 
+  <Header/>
+  <Mains/>
+  <Footer/>
+  </> 
+  ); 
+} 
+export default App;
